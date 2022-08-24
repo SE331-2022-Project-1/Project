f6dec7f64707fdcdf65f3c47dc65398b57ea3ad1
Ponarx <phonkrit_c@cmu.ac.th>
2022-08-24 23:44:33 +0700

    Show patient list in view

--- project1/src/router/index.js
+++ project1/src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventListView from "../views/EventListView.vue";
+import AboutView from "../views/AboutView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "EventListView",
+    component: EventListView,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
