65b17b69603777791082ae3b3583615f2cb40369
Natthapong_Pop <popbobkok34@hotmail.com>
2022-08-27 15:26:51 +0700

    Docter Comment Part - fixed already
    Docter can input docter name and docter comment then show to name, comment below review

--- project1/src/views/event/CommentEvent.vue
+++ project1/src/views/event/CommentEvent.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>Patient Name: {{ event.name + " " + event.surname }}</h3>
 
-  <div class="review-form">
+  <div i-d-b-key-range="review-form">
     <form class="review-form" @submit.prevent="onSubmit">
       <label for="nameD">Docter Name:</label>
       <input id="nameD" v-model="nameD" />
@@ -11,13 +11,13 @@
     </form>
   </div>
 
-  <div class="review-list">
-    <h3>Reviews</h3>
+  <div id="review-list">
+    <h3>--- Docter Comment ---</h3>
     <ul>
       <a v-for="(review, index) in reviews" :key="index">
-        {{ this.nameD }}
+        Docter: {{ review.nameD }}
         <br />
-        "{{ this.comment }}"
+        Comment: "{{ review.comment }}" <br />-----------------------<br />
       </a>
     </ul>
   </div>
@@ -31,21 +31,27 @@
 <script>
 export default {
   props: ["id", "event"],
-  reviews: {
-    type: Array,
-    required: true,
-  },
+
   data() {
     return {
-      reviews: [],
+      reviews: [
+        {
+          type: Array,
+          required: true,
+          nameD: "Pop",
+          comment: "This paitent need is healthy as always.",
+        },
+      ],
       nameD: "",
       comment: "",
     };
   },
   methods: {
+    addReview(review) {
+      this.reviews.push(review);
+      console.log(review);
+    },
     onSubmit() {
-      console.log(this.nameD);
-      console.log(this.comment);
       if (this.nameD === "" || this.comment === "") {
         alert("Review is incomplete. please fill out every filed.");
         return;
@@ -54,15 +60,11 @@ export default {
         nameD: this.nameD,
         comment: this.comment,
       };
-      this.$emit("review-submitted", docterComment);
+      this.addReview(docterComment);
 
       this.nameD = "";
       this.comment = "";
     },
-  },
-  addReview(review) {
-    this.reviews.push(review);
-    console.log(review);
   },
 };
 </script>
