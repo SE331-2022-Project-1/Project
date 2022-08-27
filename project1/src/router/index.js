4f1ae475aabd7bec09fd2732938deaeab3aeac43
Ponarx <phonkrit_c@cmu.ac.th>
2022-08-27 18:16:42 +0700

    save old position when returning to old page

--- project1/src/router/index.js
+++ project1/src/router/index.js
@@ -92,6 +92,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 router.beforeEach(() => {
   NProgress.start();
