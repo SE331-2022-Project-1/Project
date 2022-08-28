94dcb230db335e349c85e69ab6c144ce0874e96c
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-08-28 15:55:51 +0700

    fix to prod

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://my-json-server.typicode.com/SE331-2022-Project-1/Project",
+  baseURL: process.env.VUE_APP_BACKEND_URL,
   withCredentials: false,
   headers: {
     Accept: "application/json",
