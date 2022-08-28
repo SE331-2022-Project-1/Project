0769ed26d7b4df558dac57e3de822038d7b10fb4
Natthapong_Pop <popbobkok34@hotmail.com>
2022-08-28 16:07:15 +0700

    change back-end

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
-  withCredentials: false,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+    baseURL: "https://my-json-server.typicode.com/SE331-2022-Project-1/Project",
+    withCredentials: false,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
 });
 
 export default {
-  getEvents(perPage, page) {
-    return apiClient.get("/events?_limit=" + perPage + "&_page=" + page);
-  },
-  getEvent(id) {
-    return apiClient.get("/events/" + id);
-  },
+    getEvents(perPage, page) {
+        return apiClient.get("/events?_limit=" + perPage + "&_page=" + page);
+    },
+    getEvent(id) {
+        return apiClient.get("/events/" + id);
+    },
 };
